b61c25014f2978c41d800626a6618e289467c009
10kaoru12 <10kaoru12@gmail.com>
2019-02-18 00:56:44 +0900

    内容の追加

---
+++ ABC/2/b/b.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+#define REP(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define RREP(i, x, n) for (int i = x - 1; i >= (int)(n); --i)
+#define rep(i, n) REP(i, 0, n)
+#define rrep(i, n) RREP(i, n, 0)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define MAX 1e9 + 7
+
+using namespace std;
+
+int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
+int lcm(int a, int b) { return a * b / gcd(a, b); }
+
+int main(void)
+{
+    string s;
+    cin >> s;
+    int n = s.size();
+    rep(i, n)
+    {
+        if (s[i] == 'a' or s[i] == 'i' or s[i] == 'u' or s[i] == 'e' or s[i] == 'o')
+        {
+            s.erase(i, 1);
+            --i;
+        }
+    }
+    cout << s << endl;
+    return 0;
+}
