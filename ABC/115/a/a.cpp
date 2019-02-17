4e6621c4177ee92d510db921f0d748bee9ee0d67
10kaoru12 <10kaoru12@gmail.com>
2019-02-17 21:16:16 +0900

    内容の追加

---
+++ ABC/115/a/a.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+    int d;
+    cin >> d;
+    if (d == 25)
+    {
+        cout << "Christmas" << endl;
+    }
+    else if (d == 24)
+    {
+        cout << "Christmas Eve" << endl;
+    }
+    else if (d == 23)
+    {
+        cout << "Christmas Eve Eve" << endl;
+    }
+    else if (d == 22)
+    {
+        cout << "Christmas Eve Eve Eve" << endl;
+    }
+}
