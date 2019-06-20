fba3013b7220cc24b6f3f010572a5a0f7f048888
Kaoru Ishizawa <kaoru.ishizawa@nnlife.co.jp>
2019-06-20 21:14:08 +0900

    code-snippetsの修正と98b解答

---
+++ ABC/98/b/b.cpp
@@ -0,0 +1,76 @@
+/* include */
+#include <bits/stdc++.h>
+#include <boost/range/algorithm.hpp>
+#include <boost/algorithm/string/trim.hpp>
+
+/* using */
+using namespace std;
+
+/* main constructor */
+struct Fast
+{
+    Fast()
+    {
+        std::cin.tie(0);
+        ios::sync_with_stdio(0);
+        //cout << fixed << setprecision(12);
+    }
+} fast;
+
+/* template */
+template <class T>
+bool chmax(T &a, const T &b) { return (a < b) ? (a = b, 1) : 0; }
+template <class T>
+bool chmin(T &a, const T &b) { return (b < a) ? (a = b, 1) : 0; }
+
+/* define */
+#define REP(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define RREP(i, x, n) for (int i = x - 1; i >= (int)(n); --i)
+#define rep(i, n) REP(i, 0, n)
+#define reps(i, n) REP(i, 1, n + 1)
+#define rrep(i, n) RREP(i, n, 0)
+#define each(i, n) for (auto &i : n)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define eunique(x) \
+    boost::sort(x);  \
+    (x).erase(unique(all(x)), (x).end())
+#define mp make_pair
+#define r0 return 0
+
+/* const */
+const int inf = 1e9 + 7;
+const int dxl[8] = {1, 0, -1, 0, 1, -1, -1, 1};
+const int dyl[8] = {0, 1, 0, -1, 1, 1, -1, -1};
+const int dxs[4] = {1, 0, -1, 0};
+const int dys[4] = {0, 1, 0, -1};
+
+/* main */
+signed main(void)
+{
+    int n;
+    int cnt=0;
+    int max=0;
+    string s;
+    string front, end;
+    string::size_type pos;
+    cin >> n >> s;
+    REP(i, 1, n)
+    {
+        front = s.substr(0, i);
+        end = s.substr(i);
+        eunique(front);
+        rep(i,front.size()){
+            pos=end.find(front[i]);
+            if(pos!=string::npos){
+                cnt++;
+            }
+        }
+        if(max<cnt){
+            max=cnt;
+        }
+        cnt=0;
+    }
+    cout<<max<<endl;
+    return 0;
+}
